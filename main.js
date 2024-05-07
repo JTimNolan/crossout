c66c2a9b2ee258da7e2fe1a94bf846fcf7a89438
= <=>
2024-05-07 00:18:18 -0400

    Add basic layout

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 Alpine.data('crossout', () => ({
     test: 'test',
+    cells: [],
+    words: [
+        "hello",
+        "world",
+    ],
+    init(){
+        this.cells = new Array(100);
+    },
 }));
